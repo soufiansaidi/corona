beb4737a2771239f0281f8895db2645dc35edb64
soufian <soufian@MacBook-Pro-de-soufian.local>
2020-03-05 12:05:02 +0100

    adding countries table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import CountUp from 'react-countup'
 
 import Cases from './img/cases.svg';
 import Death from './img/death.svg';
@@ -14,33 +15,29 @@ const App = () => {
     .then(res => res.json())
     .then( data => setData(data) )
   }, [])
-  
-
-  console.log(data.entries.length)
 
   return (
-
     <div className="container">
       <div className="statistics-generale">
         <div className="single-static">
           <img src={ Cases } alt="الحالات" />
           <div className="wrap cases">
             <h3>الحالات</h3>
-            <div>{ data.total_cases }</div>
+            <div>{(data.total_cases) ? <CountUp end={ data.total_cases } /> : 0 }</div>
           </div>
         </div>
         <div className="single-static">
           <img src={ Death } alt="الموت" />
           <div className="wrap death">
             <h3>الموت</h3>
-            <div>{ data.total_deaths }</div>
+            <div>{(data.total_deaths) ? <CountUp end={ data.total_deaths } /> : 0 }</div>
           </div>
         </div>
         <div className="single-static">
           <img src={ Recovered } alt="تعافى" />
           <div className="wrap recovery">
             <h3>تعافى</h3>
-            <div>{ data.total_deaths }</div>
+            <div>{(data.total_recoverds) ? <CountUp end={ data.total_recoverds } /> : 0 }</div>
           </div>
         </div>
       </div>
@@ -55,9 +52,9 @@ const App = () => {
                     <li key={i}>
                       <img src={ Uae } alt="flag" />
                       <div className="country-statics">
-                        <div className="cases">{ entry.cases }</div>
-                        <div className="death">{ entry.deaths }</div>
-                        <div className="recovery">{ entry.recovered }</div>
+                        <div className="cases">{(entry.cases) ? <CountUp end={ entry.cases } /> : 0 }<div>حالات</div></div>
+                        <div className="death">{(entry.deaths) ? <CountUp end={ entry.deaths } /> : 0 }<div>موت</div></div>
+                        <div className="recovery">{(entry.recovered) ? <CountUp end={ entry.recovered } /> : 0 }<div>تعافى</div></div>
                       </div>
                     </li>
                   )

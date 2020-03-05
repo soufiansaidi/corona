642bd45ff7f02b8f034bddfed52bafaa1568f3b7
soufian <soufian@MacBook-Pro-de-soufian.local>
2020-03-05 11:39:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import Cases from './img/cases.svg';
+import Death from './img/death.svg';
+import Recovered from './img/death.svg';
+
+import Uae from './img/united-arab-emirates.svg';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://3.15.26.157:8000/api/v1/admin/sites/corona/ar")
+    .then(res => res.json())
+    .then( data => setData(data) )
+  }, [])
+  
+
+  console.log(data.entries.length)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <div className="statistics-generale">
+        <div className="single-static">
+          <img src={ Cases } alt="الحالات" />
+          <div className="wrap cases">
+            <h3>الحالات</h3>
+            <div>{ data.total_cases }</div>
+          </div>
+        </div>
+        <div className="single-static">
+          <img src={ Death } alt="الموت" />
+          <div className="wrap death">
+            <h3>الموت</h3>
+            <div>{ data.total_deaths }</div>
+          </div>
+        </div>
+        <div className="single-static">
+          <img src={ Recovered } alt="تعافى" />
+          <div className="wrap recovery">
+            <h3>تعافى</h3>
+            <div>{ data.total_deaths }</div>
+          </div>
+        </div>
+      </div>
+      {
+        (data.entries.length > 0) && 
+        (
+          <div className="countries">
+            <ul>
+              { 
+                data.entries.map(
+                  (entry, i) => (
+                    <li key={i}>
+                      <img src={ Uae } alt="flag" />
+                      <div className="country-statics">
+                        <div className="cases">{ entry.cases }</div>
+                        <div className="death">{ entry.deaths }</div>
+                        <div className="recovery">{ entry.recovered }</div>
+                      </div>
+                    </li>
+                  )
+                )
+              }
+            </ul>
+          </div>
+        )
+      }
     </div>
   );
 }
